910e7d2c6253677c1fdf7e7f63d73e07f39bd308
Muhammed Safwan K <muhammedsafwank00@gmail.com>
2023-08-04 07:25:24 +0530

    password show/hide icon is added

--- forms/signup.js
+++ forms/signup.js
@@ -75,3 +75,23 @@ function validateForm() {
     return true;
   }
 }
+
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+togglePassword.addEventListener("click", () => {
+ 
+  const type =
+  password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  togglePassword.classList.toggle("fa-eye");
+});
+
+const toggleConfirm = document.querySelector("#toggleConfirm");
+const confirmPassword = document.querySelector("#confirmPassword");
+toggleConfirm.addEventListener("click", () => {
+ 
+  const type =
+  confirmPassword.getAttribute("type") === "password" ? "text" : "password";
+  confirmPassword.setAttribute("type", type);
+  toggleConfirm.classList.toggle("fa-eye");
+});
